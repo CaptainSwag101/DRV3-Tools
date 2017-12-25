f242e7a54ad8b636b6e559652475d7514069c255
James Pelster <jpmac26@gmail.com>
2017-12-24 22:56:49 -0800

    Add an event for the "Exit" menu option

--- stx_editor/mainwindow.h
+++ stx_editor/mainwindow.h
@@ -28,6 +28,8 @@ private slots:
     void on_actionSave_triggered();
     void on_actionSave_As_triggered();
 
+    void on_actionExit_triggered();
+
 private:
     bool CheckUnsaved();
     void OpenStxFile();
