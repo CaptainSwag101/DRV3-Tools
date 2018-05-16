7d2f087da839ba1bdc6a631e997446d127b1b518
James Pelster <jpmac26@gmail.com>
2018-05-16 15:26:49 -0700

    Fix stx_editor extraction path (again)

--- stx_ex/main.cpp
+++ stx_ex/main.cpp
@@ -4,7 +4,7 @@
 #include "../utils/stx.h"
 
 void unpack(const QString in_path);
-void unpack_file(const QString in_file, const QString ex_path);
+void unpack_file(const QString in_filepath, const QString in_dirpath);
 void repack(const QString in_path);
 
 int main(int argc, char *argv[])
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 
         if (arg == "-p" || arg == "--pack")
             pack = true;
-        else
-            in_path = QDir::toNativeSeparators(QDir(argv[i]).absolutePath());
+        else if (in_path.isEmpty())
+            in_path = QDir(argv[i]).absolutePath();
     }
 
     if (in_path.isEmpty())
@@ -42,13 +42,11 @@ void unpack(const QString in_path)
 {
     if (QFileInfo(in_path).isDir())
     {
-        QString ex_path = in_path + "-ex";
-
         QDirIterator it(in_path, QStringList() << "*.stx", QDir::Files, QDirIterator::Subdirectories);
         QStringList stxNames;
         while (it.hasNext())
         {
-            stxNames.append(QDir::toNativeSeparators(it.next()));
+            stxNames.append(it.next());
         }
 
         stxNames.sort();
@@ -57,7 +55,7 @@ void unpack(const QString in_path)
             cout << "Extracting file " << (i + 1) << "/" << stxNames.size() << ": " << QFileInfo(stxNames.at(i)).fileName() << "\n";
             cout.flush();
 
-            unpack_file(stxNames.at(i), ex_path);
+            unpack_file(stxNames.at(i), in_path);
         }
     }
     else
@@ -69,75 +67,118 @@ void unpack(const QString in_path)
             return;
         }
 
-        QString ex_path = QFileInfo(in_path).dir().absolutePath() + "-ex";
-        unpack_file(in_path, ex_path);
+
+        unpack_file(in_path, in_path);
     }
 }
 
-void unpack_file(const QString in_file, const QString ex_path)
+void unpack_file(const QString in_filepath, const QString in_dirpath)
 {
-    QString rel_path = QDir::toNativeSeparators(QFileInfo(in_file).dir().relativeFilePath(in_file));
-    QString out_path = ex_path + QDir::separator() + rel_path;
+    QString ex_dirpath = in_dirpath + "-ex";
+    QString rel_filepath = QDir(in_dirpath).relativeFilePath(in_filepath);
+    QString out_filepath = ex_dirpath + '/' + rel_filepath;
+    out_filepath.replace(".stx", ".txt");
 
-    QFile f(in_file);
-    f.open(QFile::ReadOnly);
-    QByteArray data = f.readAll();
-    f.close();
-
-    int dirSeparatorIndex = QDir::toNativeSeparators(rel_path).lastIndexOf(QDir::separator());
+    int dirSeparatorIndex = rel_filepath.lastIndexOf('/');
     if (dirSeparatorIndex > 0)
-        QDir(ex_path).mkpath(QDir::toNativeSeparators(rel_path).left(dirSeparatorIndex));
+        QDir(ex_dirpath).mkpath(rel_filepath.left(dirSeparatorIndex));
 
-    QString out_file = rel_path;
-    out_file.replace(".stx", ".txt");
+    QFile in(in_filepath);
+    in.open(QFile::ReadOnly);
+    const QStringList strings = get_stx_strings(in.readAll());
+    in.close();
 
-    QStringList strings = get_stx_strings(data);
-
-    QFile textFile(ex_path + QDir::separator() + out_file);
-    textFile.open(QFile::WriteOnly);
+    QFile out(out_filepath);
+    out.open(QFile::WriteOnly);
     for (int i = 0; i < strings.size(); i++)
     {
-        textFile.write(QString("##### " + QString::number(i).rightJustified(4, '0') + "\n").toUtf8());
-        textFile.write(QString(strings.at(i) + "\n\n").toUtf8());
+        out.write(QString("##### " + QString::number(i).rightJustified(4, '0') + "\n").toUtf8());
+        out.write(QString(strings.at(i) + "\n\n").toUtf8());
     }
-    textFile.close();
-
+    out.close();
 }
 
 void repack(const QString in_path)
 {
-    QString cmp_dir = in_path + "-cmp";
-
-    /*
-    if (!QDir(cmp_dir).exists())
+    if (QFileInfo(in_path).isDir())
     {
-        if (!QDir().mkpath(cmp_dir))
+        QString cmp_dirpath = in_path + "-cmp";
+
+        if (!QDir(cmp_dirpath).exists() && !QDir().mkpath(cmp_dirpath))
         {
-            cout << "Error: Failed to create \"" << cmp_dir << "\" directory.\n";
+            cout << "Error: Failed to create \"" << cmp_dirpath << "\" directory.\n";
             cout.flush();
             return;
         }
+
+        QDirIterator it(in_path, QStringList() << "*.txt", QDir::Files, QDirIterator::Subdirectories);
+        QStringList txtFiles;
+        while (it.hasNext())
+            txtFiles.append(it.next());
+
+        txtFiles.sort();
+        for (int f = 0; f < txtFiles.count(); f++)
+        {
+            QString file = QDir(in_path).relativeFilePath(txtFiles.at(f));
+            cout << "Re-packing file " << (f + 1) << "/" << txtFiles.count() << ": \"" << file << "\"\n";
+            cout.flush();
+
+            // The original files (usually) use Unix-style line breaks.
+            // A couple files sometimes have the occasional Windows-style line break,
+            // but since these are almost certainly not intentional, we sanitize them anyway.
+            QFile txt(txtFiles[f]);
+            txt.open(QFile::ReadOnly | QIODevice::Text);
+            QStringList allText = QString(txt.readAll()).replace("\r\n", QString('\n')).replace('\r', '\n').split('\n', QString::SkipEmptyParts);
+            txt.close();
+
+            QStringList strings;
+            QString str;
+            int index = 0;
+            for (QString strPart : allText)
+            {
+                if (strPart.startsWith("#"))
+                {
+                    if (!str.isEmpty())
+                    {
+                        strings.append(str);
+                        str.clear();
+                    }
+
+                    index = strPart.replace("##### ", "").toInt();
+                    continue;
+                }
+
+                if (!str.isEmpty())
+                {
+                    str += "\n";
+                }
+                str += strPart;
+            }
+            strings[index] = str;
+            str.clear();
+            QByteArray stxData = repack_stx_strings(strings);
+
+            QString outPath = cmp_dirpath + '/' + file;
+            outPath.replace(".txt", ".stx");
+            QDir().mkpath(outPath.left(outPath.lastIndexOf('/')));
+            QFile outFile(outPath);
+            outFile.open(QFile::WriteOnly);
+            outFile.write(stxData);
+            outFile.close();
+        }
     }
-    */
-
-    QDirIterator it(in_path, QStringList() << "*.txt", QDir::Files, QDirIterator::Subdirectories);
-    QStringList txtFiles;
-    while (it.hasNext())
-        txtFiles.append(it.next());
-
-    txtFiles.sort();
-    for (int f = 0; f < txtFiles.count(); f++)
+    else
     {
-        QString file = QDir(in_path).relativeFilePath(txtFiles.at(f));
-        cout << "Re-packing file " << (f + 1) << "/" << txtFiles.count() << ": \"" << file << "\"\n";
+        QString file = in_path;
+        cout << "Re-packing file" << ": \"" << file << "\"\n";
         cout.flush();
 
         // The original files (usually) use Unix-style line breaks.
         // A couple files sometimes have the occasional Windows-style line break,
         // but since these are almost certainly not intentional, we sanitize them anyway.
-        QFile txt(txtFiles[f]);
+        QFile txt(in_path);
         txt.open(QFile::ReadOnly | QIODevice::Text);
-        QStringList allText = QString(txt.readAll()).replace("\r\n", "\n").replace("\r", "\n").split("\n", QString::SkipEmptyParts);
+        QStringList allText = QString(txt.readAll()).replace("\r\n", QString('\n')).replace('\r', '\n').split('\n', QString::SkipEmptyParts);
         txt.close();
 
         QStringList strings;
@@ -167,9 +208,9 @@ void repack(const QString in_path)
         str.clear();
         QByteArray stxData = repack_stx_strings(strings);
 
-        QString outPath = QDir::toNativeSeparators(cmp_dir + QDir::separator() + file);
+        QString outPath = file;
         outPath.replace(".txt", ".stx");
-        QDir().mkpath(outPath.left(outPath.lastIndexOf(QDir::separator())));
+        QDir().mkpath(outPath.left(outPath.lastIndexOf('/')));
         QFile outFile(outPath);
         outFile.open(QFile::WriteOnly);
         outFile.write(stxData);
